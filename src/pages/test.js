80f1e4d934cf03d8573eb081588b8c54a7d46b68
Maria <mariagiraldo4@gmail.com>
2022-05-16 22:21:08 +0000

    primer commit

---
+++ src/pages/test.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Test = () => {
+  return <div>helloo</div>
+}
+
+export default Test
