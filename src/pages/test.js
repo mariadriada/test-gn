22f00b762ddeca9d18b28e5174c198d660291f46
Maria <mariagiraldo4@gmail.com>
2022-05-20 21:18:58 +0000

    test3

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,11 +1,23 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 import { graphql } from "gatsby"
+import axios from "axios"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const Test = () => {
-  return <div>helloo</div>
+  const [data, setData] = useState("")
+  const fetchData = async () => {
+    const result = await axios.get("../api/hello-world")
+    console.log(result.data.hello)
+    setData(result.data.hello)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return <div>{data}</div>
 }
 
 export default Test
